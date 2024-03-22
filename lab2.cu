60f1199f5a8d5266b688f75c72844a3a64605e76
SienPeerlinck <sienpeerlinck@hotmail.com>
2024-03-22 09:22:20 +0100

    Update lab2.cu

--- lab2.cu
+++ lab2.cu
@@ -1,15 +1,18 @@
+
 #include <iostream>
 #include <cuda_runtime.h>
 #include <chrono>
 
-#define N 96 // Size of the array
+
+#define N 96 // Size of the arrays
 
 
 __global__ void GPUatomicmax(int *input, int *max2, int size){
-    int tid = blockIdx.x * blockDim.x + threadIdx.x;  // total index = number of the block we're in now x number of threads + number of the thread in the current block
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;  // totale index = nummer van blok waar we nu inzitten x aantal blokken + nummer van de thread
     int stride = blockDim.x*gridDim.x;
 
-    int max = 0;
+    __shared__ int max;
+    max = 0;
 
     for(int i = tid; i<size; i+=stride){
         atomicMax(&max,  input[i]);
@@ -19,9 +22,22 @@ __global__ void GPUatomicmax(int *input, int *max2, int size){
 }
 
 __global__ void GPUreductionmax(int *input, int *max3, int size){
+    //int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    //int stride = blockDim.x*gridDim.x;
+
+    //int sdata[];
+
     unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    //sdata[tid] = (i < size) ? input[i] : INT_MIN;
     __syncthreads();
 
+    //for (unsigned int s = blockDim.x; s > 0; s >>= 1) {
+    //    if (tid < s) {
+    //        input[tid] = max(input[tid], input[tid + s]);
+    //    }
+    //    __syncthreads();
+    //}
     for (int s = 1; s < size/2 ; s++) {
         if (tid < s) {
             input[s*tid] = max(input[s*tid], input[s*(tid + 1)]);
@@ -29,19 +45,21 @@ __global__ void GPUreductionmax(int *input, int *max3, int size){
         __syncthreads();
     }
      *max3 = input[0];
+    //*max3 = 5;
      //relatie tussen de verschillende -> bij de eerste is *2 en +1, volgende is *4 en +2 , volgende ...
      
 }
 
+
+
+
+
 // CPU code
 int main() {
-    // Creation of the input array of size N (defined on line 5) filled with random integers
     int inputArray[N];
     for (int i=0; i<N; i++){
         inputArray[i] = rand();
     }
-
-    // Allocating a memoryplace for the input array, putting it there and moving it to the GPU memory
     int *d_inputArray;
     cudaMalloc(&d_inputArray, N * sizeof(int)); 
     cudaMemcpy(d_inputArray, inputArray, N * sizeof(int), cudaMemcpyHostToDevice); // cpu memory to GPU memory
@@ -50,7 +68,8 @@ int main() {
     int blockSize = 256;  // aantal threads in block
     int gridSize = (N + blockSize - 1) / blockSize;
 
-    // Nested for-loop on CPU
+
+    //CPU
     int maxcpu = 0;
     const auto start = std::chrono::steady_clock::now();
 
@@ -74,10 +93,12 @@ int main() {
     GPUatomicmax<<<gridSize, blockSize>>>(d_inputArray, d_maxgpu1, N);  // geef pointers naar arrays mee
     cudaMemcpy(&maxgpu1, d_maxgpu1, sizeof(int), cudaMemcpyDeviceToHost); // cpu memory to GPU memory
 
+    //cudaFree(d_inputArray);
     cudaFree(d_maxgpu1);
     
     const auto endgpu1 = std::chrono::high_resolution_clock::now();
     const std::chrono::duration<double> elapsed_seconds_gpu{endgpu1 - startgpu1};
+
 
     //GPU reduction
     int maxgpu2 = 0;
